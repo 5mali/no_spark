023de231084c8bce95a4ec899289b00c119023c8
5mali <shaswot@gmail.com>
2019-02-26 01:10:09 +0900

    failed ist attempts

--- parallel_seed_test.sh
+++ parallel_seed_test.sh
@@ -7,7 +7,7 @@ do
 		rm ./seed_output/${name}.out
 	fi
 	
-	parallel -k python ::: ./py_scripts/${name}.py ::: 0 1 2 3 4 5 6 7 8 9 > ./seed_output/${name}.out
+	parallel -k -j15 python ::: ./py_scripts/${name}.py ::: 0 1 2 3 4 5 6 7 8 9 > ./seed_output/${name}.out
 
 
 #	for seed_index in {0..9} 

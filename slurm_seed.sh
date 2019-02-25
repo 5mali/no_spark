d4cbfb72fce132aa06aeda87f92178fc09134e7a
5mali <shaswot@gmail.com>
2019-02-25 19:04:36 +0900

    fixed parallel script

--- slurm_seed.sh
+++ slurm_seed.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-#SBATCH --job-name=${1}
-#SBATCH --output=${1}job.txt
+######SBATCH --job-name=$(1)
+######SBATCH --output=${1}job.txt
 #SBATCH -p knm
 ###### #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
-#SBATCH --time=60:00
-##### #SBATCH --mem-per-cpu=100
+######SBATCH --time=60:00
+###### #SBATCH --mem-per-cpu=100
 
 #srun hostname
 #srun sleep 60
@@ -16,4 +16,5 @@
 #srun pwd
 #srun cd ~/repos/no_spark
 source activate eno
-srun ~/repos/no_spark/seed_test.sh ${1} 
+echo $1
+srun ~/repos/no_spark/parallel_seed_test.sh ${1} 

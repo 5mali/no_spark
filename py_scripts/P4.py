361ff1d843c6d1f88afa99349d17a72d8ca73807
5mali <shaswot@gmail.com>
2019-02-26 18:23:17 +0000

    P4

--- py_scripts/P4.py
+++ py_scripts/P4.py
@@ -63,7 +63,7 @@ os.environ['PYTHONHASHSEED'] = str(seed)
 # In[ ]:
 
 
-NAME       = 'P3'
+NAME       = 'P4'
 MODELNAME  = NAME + '_' + str(seed) + '.pt'
 print("\nMODEL : ", NAME)
 print("SEED  : ",seed_arg)

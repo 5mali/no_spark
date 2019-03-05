86bb136ef1b8eb50fec07a4eb0dc7ac2892677f5
5mali <shaswot@gmail.com>
2019-03-05 17:17:31 +0900

    S and T

--- batch_run.sh
+++ batch_run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 #SBATCH --job-name=batch_run
-#SBATCH --output=batch_run_T2.out
+#SBATCH --output=batch_run_U1.out
 #SBATCH -p p
 #SBATCH -N 1
-#SBATCH --cpus-per-task 10
+##SBATCH --cpus-per-task 10
+#SBATCH c 10
 source activate eno
 srun echo "Number of cores: "
 srun parallel --number-of-cores
